2dad643d5ba4ba279321639ff0c44ade73656f78
andrewowen <andrew@moduscreate.com>
2016-05-06 11:18:42 -0400

    modify users with current employees

--- example/employeeData.module.js
+++ example/employeeData.module.js
@@ -3,8 +3,8 @@
 module.exports = {
 	twitterInfo: {
 		'Andrew Owen': {handle: 'modusAndrew'},
-		'Steve Dalgetty': {handle: 'stevedalgetty'},
 		'Dave Ackerman': {handle: 'dmackerman'},
+		'JD Jones': {handle: 'Jaymes_Jones'}
 		//This one should fail to show you that the test is catching fake employees
 		// 'Fake Jay Garcia': {handle: 'notjaygarcia'}
 
